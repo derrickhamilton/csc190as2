a1ee35d6a8ec5cb934bb86e95278a1719614a079
Derrick Hamilton <derrickhamilton212@gmail.com>
2018-03-10 21:24:21 -0500

    Created simple pages in list.php and report.php

--- list.php
+++ list.php
@@ -1,3 +1,13 @@
 <?php
     //this is list.php
 ?>
+
+<head>
+  <title>Dwiddder | List</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <h1>This is list.php</h1>
+</body>
