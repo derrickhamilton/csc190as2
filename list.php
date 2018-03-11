6449aea32fe9449b4369adda95ff5a271687da8e
Derrick Hamilton <derrickhamilton212@gmail.com>
2018-03-11 16:55:46 -0400

    fixed database connection bug

--- list.php
+++ list.php
@@ -1,7 +1,7 @@
 <?php
     include('config.php');
 
-    $conn = new mysqli($db_host, $db_name, $db_user, $db_pass);
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
     if ($conn->connect_error) {
 	die('Connection failed: '.$conn->connect_error.'<br>');
     }
