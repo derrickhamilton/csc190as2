e8443e91ef41f7ec23a68b1434349e80a8442914
Mustafa Ghadiyali <ghadiyalimustafa@gmail.com>
2018-03-10 20:52:20 -0500

    Edited Database SCHEMA

--- db.sql
+++ db.sql
@@ -1,1 +1,1 @@
-a database thing
+CREATE TABLE reports (id smallint unsigned not null auto_increment, name varchar(55) not null, report varchar(252) not null, date varchar(55) not null, primary key (id));
